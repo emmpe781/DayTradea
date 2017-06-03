dc14e77c907c2d2ff803665916355f3773d27cfa
Emma Persson <emmpe781@gmail.com>
2017-06-03 12:29:29 +0200

    testing testing

--- src/hello.cpp
+++ src/hello.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello Worlllllld!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Worlleeeeeeelllld!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
